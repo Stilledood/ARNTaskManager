74bc26607669daba6bcd5326964f89caaa94e0c8
Stilledood <corciova.f@gmail.com>
2023-09-20 17:51:58 +0300

    Created custom user class

--- ArnTask/ArnTask/settings.py
+++ ArnTask/ArnTask/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party apps
+    # Local apps
+    'Task',
+    'Users'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "Users.User"
